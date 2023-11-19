b8be5c966aefd4167931a9c2963644fea02210fc
Gabriel Santana Santos <gabrielsantana123santos@gmail.com>
2023-11-19 03:08:01 -0300

    Começando a mexer com a API

--- App.js
+++ App.js
@@ -5,6 +5,22 @@ import HistoryGraphic from './src/components/HistoryGraphic';
 import QuotationsList from './src/components/QuotationsList';
 import QuotationsItems from './src/components/QuotationsList/QuotationsItems';
 
+function addZero(number){
+  if(number <= 9) {
+    return "0" + number;
+  }
+  return number;
+}
+
+function url(qtdDays) {
+  const date = new Date();
+  const listLastDays = qtdDays ;
+  const end_date = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addzero(date.getDay())}` ;
+  date.setDate(date.getDate() - listLastDays);
+  const start_date = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addzero(date.getDay())}` ;
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?star=${start_date}&end=${end_date}`
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
